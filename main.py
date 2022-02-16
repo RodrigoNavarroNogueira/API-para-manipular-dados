f8215fbb17435b4772b235212e771a93153b63f7
RodrigoNavarroNogueira <rodrigo.navarro36@hotmail.com>
2022-02-16 19:17:00 -0300

    Adicionando a função para consultar a tabela TV

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-import pandas as pd 
-from flask import Flask, jsonify 
+import pandas as pd  
+from flask import Flask, jsonify  
 
 app = Flask(__name__)   
 
 
-@app.route('/')  
-def homepage():  
+@app.route('/')   
+def homepage():    
     return 'Essa é a homepage do site'
 
 
 @app.route('/pegarvendas')
 def pegar_vendas():
-    table = pd.read_csv('Criando-API-no-Python.csv') 
+    table = pd.read_csv('Criando-API-no-Python.csv')  
     total_vendas = table['Vendas'].sum()  
     resposta = {'total de vendas': total_vendas}  
     return jsonify(resposta) 
 
 
-app.run()
+@app.route('/pegarvendasum')
+def pegar_tv():
+    table = pd.read_csv('Criando-API-no-Python.csv')
+    total_tv = table['TV'].sum()
+    resposta = {'total de vendas da tv': total_tv}
+    return jsonify(resposta)
+
+
+app.run()  
